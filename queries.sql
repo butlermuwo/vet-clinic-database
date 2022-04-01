d63a2fa5a14f27673711c80253cf8f4accd26810
unknown <butlermuwo@gmail.com>
2022-04-01 21:07:44 +0200

    Add query and update

--- queries.sql
+++ queries.sql
@@ -8,3 +8,41 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 and 17.3;
+
+BEGIN;
+UPDATE animals 
+SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE name like '%mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+COMMIT;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals
+ROLLBACK;
+SELECT * FROM animals
+
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth > '01/01/2022';
+SAVEPOINT delete_by_birth;
+UPDATE animals
+SET weight_kg = weight_kg * (-1);
+ROLLBACK TO delete_by_birth;
+UPDATE animals
+SET weight_kg = weight_kg * (-1)
+WHERE weight_kg < 0;
+COMMIT;
+SELECT * FROM animals;
+/* End transaction */
+
